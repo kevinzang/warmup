2be2180380f8785c969e3bfd068ab559122ddc09
Kevin <kevinzang@berkeley.edu>
2014-02-08 19:24:47 -0800

    test

--- bin/testUsersModel.rb
+++ bin/testUsersModel.rb
@@ -1,5 +1,5 @@
 #!/usr/bin/env ruby
-require File.expand_path("../../app/models/user_data.rb", __FILE__)
+load File.expand_path("../../config/environments/production.rb", __FILE__)
 puts "Hello"
 u = UserData.new("Kevin Zhang")
 u.save()
