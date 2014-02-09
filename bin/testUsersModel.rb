701f08cc46756a0b9e02738ebf4a45d4f1f56f56
Kevin <kevinzang@berkeley.edu>
2014-02-08 19:20:29 -0800

    test

--- bin/testUsersModel.rb
+++ bin/testUsersModel.rb
@@ -1,17 +1,7 @@
 #!/usr/bin/env ruby
-failed = 0
-tests = []
-
-def test_1()
-	UsersModel.TESTAPI_resetFixture()
-	if UsersModel.SUCCESS == UsersModel.add("kevin", "zhang")
-		return ""
-	else
-		return "Failed: user name is valid"
-		failed += 1
-	end
-end
-
-test_1()
-puts failed
+require "../../app/models/user_data.rb"
+puts "Hello"
+u = UserData.new("Kevin Zhang")
+u.save()
+puts "DONE"
 
